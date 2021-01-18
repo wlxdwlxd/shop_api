8ded6d3beb99945da611802cd1f13cd78fb06d1b
wlxd <w2533886738@163.com>
2021-01-18 12:03:29 +0800

    sku_value最终版本

--- src/main/java/com/fh/shop_api/api/dao/SkuValueDao.java
+++ src/main/java/com/fh/shop_api/api/dao/SkuValueDao.java
@@ -12,12 +12,12 @@ public interface SkuValueDao {
     @Select(" select * from shop_attribute_value where 1=1 and attrId=#{skuId} and isDel=0")
     List<SkuValue> querySkuValue(Integer skuId);
 
-    @Insert(" insert into shop_attribute_value (name,nameCH,attrId) values(#{name},#{nameCH},#{attrId})")
+    @Insert(" insert into shop_attribute_value (name,nameCH,attrId,isDel) values(#{name},#{nameCH},#{attrId},0)")
     void addSkuValue(SkuValue skuValue);
 
     @Delete(" update  shop_attribute_value set isDel=1 where id=#{skuId}")
     void deleteSku(Integer skuId);
 
-    @Update(" update shop_attribute_value set name=#{name},nameCH=#{nameCH},attrId=#{attrId} where id=#{id}")
+    @Update(" update shop_attribute_value set name=#{name},nameCH=#{nameCH},attrId=#{attrId},isDel=0 where id=#{id}")
     void updateSku(SkuValue skuValue);
 }
